85fdf62d4770cba17854460b95140ed55f15d574
Mohsen <mohsen.bernaoui@gmail.com>
2023-11-19 18:28:43 +0100

    Initial commit

---
+++ src/test/java/tn/esprit/micro_service/MicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.micro_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

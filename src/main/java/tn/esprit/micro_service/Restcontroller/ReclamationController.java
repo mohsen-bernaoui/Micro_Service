c60a1177005627055893c4eebe22854cdd4ecc58
BAferiel <124399035+BAferiel@users.noreply.github.com>
2023-11-19 21:06:40 +0100

    SecondCommitReclamation

--- src/main/java/tn/esprit/micro_service/Restcontroller/ReclamationController.java
+++ src/main/java/tn/esprit/micro_service/Restcontroller/ReclamationController.java
@@ -1,9 +1,49 @@
 package tn.esprit.micro_service.Restcontroller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tn.esprit.micro_service.Entities.Reclamation;
+import tn.esprit.micro_service.Services.ReclamationServiceIMP;
+
+import java.util.List;
+
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/reclamations")
 public class ReclamationController {
+
+private ReclamationServiceIMP reclamationServiceIMP;
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Reclamation> createReclamation(@RequestBody Reclamation reclamation) {
+        return new ResponseEntity<>(reclamationServiceIMP.createReclamation(reclamation), HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Reclamation> updateReclamation(@PathVariable Long id, @RequestBody Reclamation reclamation) {
+        return new ResponseEntity<>(reclamationServiceIMP.updateReclamation(reclamation), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> deleteReclamation(@PathVariable Long id) {
+        reclamationServiceIMP.deleteReclamation(id);
+        return new ResponseEntity<>("Reclamation with ID " + id + " deleted successfully", HttpStatus.OK);
+    }
+
+    @GetMapping("/getAllReclamations")
+    public List<Reclamation> getAllReclamation() {
+        return reclamationServiceIMP.getAllReclamation();
+    }
+
+    @GetMapping("/getReclamationById/{id}")
+    public Reclamation getReclamationById(@PathVariable Long id) {
+        return reclamationServiceIMP.getReclamationById(id);
+    }
 }
--- src/main/java/tn/esprit/micro_service/Services/ReclamationServiceIMP.java
+++ src/main/java/tn/esprit/micro_service/Services/ReclamationServiceIMP.java
@@ -1,32 +1,38 @@
 package tn.esprit.micro_service.Services;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 import tn.esprit.micro_service.Entities.Reclamation;
+import tn.esprit.micro_service.Repository.ReclamationRepo;
 
 import java.util.List;
-
+@Service
+@AllArgsConstructor
 public class ReclamationServiceIMP implements IReclamation{
+    private ReclamationRepo reclamationRepo;
     @Override
     public Reclamation createReclamation(Reclamation reclamation) {
-        return null;
+        return reclamationRepo.save(reclamation);
     }
 
     @Override
     public Reclamation getReclamationById(Long idr) {
-        return null;
+        return reclamationRepo.findById(idr).orElse(null);
     }
 
     @Override
     public List<Reclamation> getAllReclamation() {
-        return null;
+        return reclamationRepo.findAll();
     }
 
     @Override
     public Reclamation updateReclamation(Reclamation reclamation) {
-        return null;
+        return reclamationRepo.save(reclamation);
     }
 
     @Override
     public void deleteReclamation(Long idr) {
+        reclamationRepo.deleteById(idr);
 
     }
 }
